eb84fdcb6d4bce1d23903259c0998d3281b79297
DESKTOP-7NCVNAM\HOANG ANH <nghoanganh19@gmail.com>
2017-12-19 10:35:06 +0700

    add column: giaVe, tongTien delete colunm: soGhe

--- DataModel/Models/DatVe.cs
+++ DataModel/Models/DatVe.cs
@@ -2,26 +2,33 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
 
     public partial class DatVe
     {
+        public DatVe()
+        {
+            ChiTietDatVes = new HashSet<ChiTietDatVe>();
+        }
+
         [Key]
         public int maDatVe { get; set; }
 
-        public int maChuyenXe { get; set; }
+        public int? maChuyenXe { get; set; }
 
-        [StringLength(5)]
-        public string soGhe { get; set; }
+        public int? maKhachHang { get; set; }
 
-        public int giaVe { get; set; }
+        public double? tongTien { get; set; }
+
+        public DateTime? ngayDat { get; set; }
 
         [StringLength(20)]
         public string trangThai { get; set; }
 
-        public int maKhachHang { get; set; }
+        public virtual ICollection<ChiTietDatVe> ChiTietDatVes { get; set; }
 
         public virtual ChuyenXe ChuyenXe { get; set; }
 
